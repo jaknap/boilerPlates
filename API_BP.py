6d110bce147bc024adb68c57b0759464a53db485
Pankaj Uchil <pankz9@gmail.com>
2017-08-22 18:26:28 -0400

    Delete API_BP.py

--- API_BP.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-
-url='API-W/-KEY'
-reponse = requests.get(url,  headers={"Content-Type": "application/json"})
-dataCt = json.loads(reponse.text)
-
-for data in jsonCategory:
-    print(data['fields'])
